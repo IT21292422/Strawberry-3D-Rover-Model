f3bb05cefd94e130fda8043a62a6e34fdbd159fc
IT21292422 <it21292422@my.sliit.lk>
2024-12-01 22:38:20 +0530

    feat: Init 3D Modal Of Rover

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.scss';
+import ThreePotScene from './components/threePotScene'
+import TitleBar from './components/titleBar'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <TitleBar />
+      </header>
+      <main>
+        <ThreePotScene />
+      </main>
+    </div>
+  );
+}
+
+export default App;
